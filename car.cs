b31f9f1892dc6420a7af16a546780f6c4185acb1
Adela <adela.yohana@gmail.com>
2020-02-24 18:09:58 -0800

    Add boilerplate code

--- car.cs
+++ car.cs
@@ -1,9 +1,18 @@
 using System;
 using System.Collections.Generic;
 
-public class Car
+public class Car // declaration of a global class
+// public = an access modifier determining this info can be accessed globally/publicly
 {
   public string MakeModel;
   public int Price;
   public int Miles;
+}
+
+public class Program
+{
+  public static void Main()
+  {
+    
+  }
 }
