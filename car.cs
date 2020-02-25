6b8afd8484a5aa4134d6fd003fe26f01feabba44
Student <example@example.com>
2020-02-25 11:36:32 -0800

    Add getter methods

--- car.cs
+++ car.cs
@@ -1,22 +1,38 @@
 namespace Dealership.Models
 {
   public class Car // declaration of a global class
-  // public = an access modifier determining this info can be accessed globally/publicly
+  // public = an access level modifier determining this info can be accessed globally/publicly
   {
-    public string MakeModel;
-    public int Price;
-    public int Miles;
+    private string _makeModel;
+    private int _price;
+    private int _miles;
+    // Naming private variables should be: _camelCase
 
     public Car(string makeModel, int price, int miles) // a constructor to "Car" class
     {
-      MakeModel = makeModel;
-      Price = price;
-      Miles = miles;
+      _makeModel = makeModel;
+      _price = price;
+      _miles = miles;
+    }
+
+    public string GetMakeModel()
+    {
+      return _makeModel;
+    }
+
+    public int GetPrice()
+    {
+      return _price;
+    }
+
+    public int GetMiles()
+    {
+      return _miles;
     }
 
     public bool WorthBuying(int maxPrice)
     {
-      return (Price < maxPrice);
+      return (_price < maxPrice);
     }
   }
 }
